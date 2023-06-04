4a08bdca860155b2c275e3b05f9b6568af0cc64a
Q <q81188118@gmail.com>
2023-06-04 15:22:43 +0000

    Update 3 files

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var cors = require('cors');
-var multer = require('multer');
-var upload = multer({ dest: 'uploads/' })
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 // connect to mongodb
 const { MongoClient } = require("mongodb");
@@ -20,23 +20,24 @@ const database = new MongoClient(process.env.MONGO_URI, {
 });
 database.connect();
 
-app.post('/api/fileanalyse', upload.single('upfile'), function(req,res) {
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
   var file = req.file;
-  var meta = { name: file.orginalname, 
+  var meta = {
+    name: file.originalname,
     type: file.mimetype,
-    size: file.size
-  }
-  const dataObj = database.db("file_metadata")
-  dataObj.collection("files").insertOne( meta, function(err, res) {
-    if(err) throw err;
-    console.log("file inserted successfully!")
-    database.close()
-  })
-    
-  res.json(meta)  
-})
+    size: file.size,
+  };
+  const dataObj = database.db("file_metadata");
+  dataObj.collection("files").insertOne(meta, function (err, res) {
+    if (err) throw err;
+    console.log("file inserted successfully!");
+    database.close();
+  });
+
+  res.json(meta);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
