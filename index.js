d2e95a8f9a41602dded67bf23f2662aced61b5af
Q <q81188118@gmail.com>
2023-06-04 15:15:06 +0000

    Update 2 files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
 var app = express();
@@ -10,9 +12,29 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+// connect to mongodb
+const { MongoClient } = require("mongodb");
+const database = new MongoClient(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+database.connect();
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function(req,res) {
+  var file = req.file;
+  var meta = { name: file.orginalname, 
+    type: file.mimetype,
+    size: file.size
+  }
+  const dataObj = database.db("file_metadata")
+  dataObj.collection("files").insertOne( meta, function(err, res) {
+    if(err) throw err;
+    console.log("file inserted successfully!")
+    database.close()
+  })
+    
+  res.json(meta)  
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
